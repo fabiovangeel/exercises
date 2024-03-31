5634100b82e08c4e0f99e8809a53a2730b878015
Fabio Vangeel <fabio.vangeel@student.ucll.be>
2024-03-31 15:49:11 +0200

    regex exercises finished

--- 03-regex/02-capturing/02-parse-link/student.py
+++ 03-regex/02-capturing/02-parse-link/student.py
@@ -1,1 +1,11 @@
-# Write your code here
+import re
+
+
+def parse_link(string):
+    match = re.fullmatch(r'<a href="(.*)">(.*)</a>', string)
+
+    if match:
+        link, caption = match.groups()
+        return (str(caption), str(link))
+    else:
+        return None
